ec2ea73d2fdf7d0a5fc6c57e156153ba1376e755
Nicolas Spycher <nic.spycher@gmail.com>
2015-03-23 14:34:36 +0100

    Added vertical smearing and combining algorithm

--- Text Line Extraction/line_extraction.py
+++ Text Line Extraction/line_extraction.py
@@ -3,7 +3,7 @@ from PIL import Image
 im = Image.open("DC/DC1/DC1.1/3243a181-1.jpg")
 im = im.convert("1")
 
-def smearing(image, limit):
+def horizontalSmearing(image, limit):
     pic = image.load()
     white = 0
     for y in range(0, image.size[1]):
@@ -18,5 +18,42 @@ def smearing(image, limit):
                 white = 0
     return image
 
-processedImage = smearing(im, 2)
-processedImage.save("processed.png")
+def verticalSmearing(image, limit):
+    pic = image.load()
+    white = 0
+    for y in range(0, image.size[0]):
+        white = 0
+        for x in range(0, image.size[1]):
+            if pic[y,x] == 255:
+                white += 1
+            elif pic[y,x] == 0 or x == image.size[0]:
+                if white >= limit and white < 50:
+                    for i in range(x, x-white-1, -1):
+                        pic[y,i] = 0
+                white = 0
+    return image
+
+def combine(image1, image2):
+    pic1 = image1.load()
+    pic2 = image2.load()
+    newImage = Image.new("1", (image1.size[0], image1.size[1]), "white")
+    combinedImage = newImage.load()
+    for y in range(0, image1.size[1]):
+        for x in range(0, image1.size[0]):
+            combined = pic1[x,y] + pic2[x,y]
+            if combined == 0:
+                combinedImage[x,y] = 0
+    return newImage
+
+
+
+
+horizontalImage = horizontalSmearing(im, 2)
+horizontalImage.save("horizontal_processed.png")
+
+verticalImage = verticalSmearing(horizontalImage, 3)
+verticalImage.save("vertical_processed.png")
+
+combinedImage = combine(horizontalImage, verticalImage)
+combinedImage.save("combined_processed.png")
+
