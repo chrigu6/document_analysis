8f619d9d5b7fc58cca425a7b3e6ceb97b3afb871
Chrigu <chrigu6@gmx.ch>
2015-04-19 17:20:43 +0200

    up

--- Exercise3/exercise3.py
+++ Exercise3/exercise3.py
@@ -8,13 +8,13 @@ import random
 import math
 import operator
 
-def loadDataset(filename, split, trainingSet=[], testSet=[]):
+def loadDataset(filename, split, trainingSet=[], testSet=[], sequenceStart=0, length=0):
     with open (filename, 'rb') as csvfile:
         lines = csv.reader(csvfile)
         dataset = list(lines)
         #x = numer of entries in a dataset, y = number of datafields in an entry In this case we are only interested in the first 4 fields
         for x in range(len(dataset)-1):
-            for y in range(4):
+            for y in range(sequenceStart, sequenceStart + length):
                 dataset[x][y] = float(dataset[x][y])
             if random.random() < split:
                 trainingSet.append(dataset[x])
@@ -66,7 +66,7 @@ def main():
     trainingSet=[]
     testSet=[]
     split = 0.67
-    loadDataset('iris.data', split, trainingSet, testSet)
+    loadDataset('iris.data', split, trainingSet, testSet, 0, 4)
     print 'Train set: ' + repr(len(trainingSet))
     print 'Test set: ' + repr(len(testSet))
     # generate predictions
@@ -100,11 +100,7 @@ def appendValueToDataSet(filename, function):
             entry.append(function(entry[1]))
             writer.writerow(entry)
         
-        
-        
-            
-        
-        
+               
             
 def getValueOfChar(string):
     result = 0
@@ -112,5 +108,4 @@ def getValueOfChar(string):
         result += ord(char)
     return result
                 
-txtToCSVParser('WashingtonDB.txt') 
-appendValueToDataSet('WashingtonDB.txt.data', getValueOfChar)
+main()
