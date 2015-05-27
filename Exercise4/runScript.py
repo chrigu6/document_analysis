126aef763a78cdb848da3ed31348a58342b2a5a4
Chrigu <chrigu6@gmx.ch>
2015-05-27 19:10:09 +0200

    merge

--- Exercise4/runScript.py
+++ Exercise4/runScript.py
@@ -8,8 +8,12 @@ import Image
 import os
 from random import randint
 import features
+from cv2 import inRange
 
-
+inputTrainingFile = "mnist" + "." + "train.noClue" + ".txt"
+inputTestFile = "mnist" + "." + "test.noClue" + ".txt"
+outputTrainingFile = "output_training.txt"
+outputResultsFile = "output_results.txt"
 
 
 
@@ -95,86 +99,47 @@ def generateOutput(values, fileName):
 
 def executeJar(inputTraining, inputTest, outputTraining, outputResults, activationFunction, featureSize, numberNeurons, outputSize, learningRate, numberEpochs):
     subprocess.call(["java", "-jar", "-Xmx512m", "NN Tool.jar","-a", getActivationFunciton(activationFunction), "-f",str(featureSize), "-n",str(numberNeurons), "-o", str(outputSize), "-l", str(learningRate), "-e", str(numberEpochs), inputTrainingFile, inputTestFile, outputTrainingFile, outputResultsFile])
-    
 
-
-if __name__ == '__main__':
+def run(numberOfNeurons, learningRate, numberOfEpochs):
     pathToImages = "/home/chrigu/Desktop/images/images/"
-    inputTrainingFile = "input_training.txt"
-    inputTestFile = "input_test.txt"
-    outputTrainingFile = "output_training.txt"
-    outputResultsFile = "output_results.txt"
     statsFile = "stats.txt"
+    outputSize = 10
+    featureSize = 8
     
     if not os.path.isfile(inputTrainingFile):
         print "extracting features"
         f = features.Features(pathToImages)
         trainFeatures = f.get_specific_feature(squares=8)
         testFeatures = f.get_specific_feature(squares=8, folder="test")
+        
         print "generating training and testing file"
-        """ generateOutput(features[0], inputTrainingFile)
-        print "generating testing file"
-        generateOutput(features[1], inputTestFile)"""
+        
         for item in trainFeatures:
             f.write_feature_vector(item[0], item[1], item[2], "noClue")
         
         for item in testFeatures:
             f.write_feature_vector(item[0], item[1], item[2], "noClue")
             
-            
+    print "number of neurons: " + str(numberOfNeurons) + "\nlearning rate: " + str(learningRate) + "\nnumber of epoches: " + str(numberOfEpochs)
     
-    featureSize = 784
-    numberOfNeurons = 5
-    outputSize = 10
-    learningRate = 0.0001
-    numberOfEpochs = 5
-    
-    accuracies = []
-    neuronValues = []
-    learningValues = []
-    epochValues = []
-    
-    c = 0
-    i = 5
-    
-    while (numberOfNeurons<4001):      
-        changeLearningRate = True
-        changeEpochs = True
-        improvment = 0
-        decreasment = 0
-        
-        while(learningRate < 1):
-            while(numberOfEpochs < 250):
-                print "number of neurons: " + str(numberOfNeurons) + "\nlearning rate: " + str(learningRate) + "\nnumber of epoches: " + str(numberOfEpochs)
-    
-                activationFunction = "SIGMOID"
-                numberOfNeurons = i
-                executeJar(inputTrainingFile, inputTestFile, outputTrainingFile, outputResultsFile, activationFunction, featureSize, numberOfNeurons, outputSize, learningRate, numberOfEpochs)
-                with open(outputResultsFile, "r") as f:
-                    line = f.readlines()
-                    line =  line[len(line)-1]
-                    accuracy = line.split(" ")[1]
-                    with open(statsFile, "a") as stats:
-                        stats.write(str(accuracy)+","+activationFunction+","+str(numberOfNeurons)+","+str(learningRate)+","+str(numberOfEpochs)+"\n")
+    activationFunction = "SIGMOID"
+    executeJar(inputTrainingFile, inputTestFile, outputTrainingFile, outputResultsFile, activationFunction, featureSize, numberOfNeurons, outputSize, learningRate, numberOfEpochs)
+    with open(outputResultsFile, "r") as f:
+        line = f.readlines()
+        line =  line[len(line)-1]
+        accuracy = line.split(" ")[1]
+        with open(statsFile, "a") as stats:
+            stats.write(str(accuracy)+","+activationFunction+","+str(numberOfNeurons)+","+str(learningRate)+","+str(numberOfEpochs)+"\n")    
+
+
+if __name__ == '__main__':
+    numberOfNeurons = 10
+    learningRate = 0.1
+    i = 2
+    while(i<513):
+        run(numberOfNeurons, learningRate, i)
+        i = i *2
                 
-                accuracies.append(accuracy)
-                neuronValues.append(numberOfNeurons)
-                learningValues.append(learningRate)
-                epochValues.append(epochValues)
-            
-                
-                numberOfEpochs = numberOfEpochs * 2
-                
-            
-            if(learningRate<0.1):
-                learningRate = learningRate * 10
-            else:
-                learningRate = learningRate + 0.2
-            numberOfEpochs = 5
-            
-        learningRate = 0.0001
-        numberOfEpochs = 5
-        numberOfNeurons = numberOfNeurons * 2
             
                 
             
